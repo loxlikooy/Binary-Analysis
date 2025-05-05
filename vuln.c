40abc7f715b55772d88a57b1fcb40efa8dbefed0
loxlikooy <58352668+loxlikooy@users.noreply.github.com>
2025-05-05 19:24:39 +0500

    1

--- vuln.c
+++ vuln.c
@@ -15,13 +15,16 @@ void another_secret() {
 void vulnerable() {
     char buffer[64];
     printf("Enter your name: ");
-    gets(buffer);  // уязвимая функция
+    gets(buffer);  // vulnerable function
     printf("Hello, %s\n", buffer);
 }
 
 int main() {
     printf("Binary Exploitation Demo\n");
     printf("------------------------\n");
+    printf("warning: this program uses gets(), which is unsafe.\n");
+    printf("Address of secret function: %p\n", secret);
+    printf("Address of another_secret function: %p\n", another_secret);
     vulnerable();
     return 0;
 } 
